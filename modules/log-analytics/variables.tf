c9a1a3fba9d47c4be6345e4eecdd5d66d457a690
EdoAbarca <eduardo.abarca.c@usach.cl>
2025-12-13 18:39:16 -0300

    [PFM-120] feat: add Log Analytics module
    Create Azure Log Analytics workspace module for centralized monitoring and logging. Includes configurable retention period and SKU options to support different compliance and monitoring requirements.

---
+++ modules/log-analytics/variables.tf
@@ -0,0 +1,44 @@
+variable "workspace_name" {
+  description = "Name of the Log Analytics workspace"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "sku" {
+  description = "SKU for the workspace"
+  type        = string
+  default     = "PerGB2018"
+}
+
+variable "retention_in_days" {
+  description = "Retention period in days"
+  type        = number
+  default     = 30
+}
+
+variable "enable_container_insights" {
+  description = "Enable Container Insights solution"
+  type        = bool
+  default     = true
+}
+
+variable "enable_sql_analytics" {
+  description = "Enable SQL Analytics solution"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
